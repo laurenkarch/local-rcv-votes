19458247f36272f0b15bf186d9be5fba9ac50594
laurenkarch <103436917+laurenkarch@users.noreply.github.com>
2022-12-04 17:09:21 -0500

    yay boyd to the rescue!

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@
 
     }
 
-    var mymap = L.map("map", {
+    var map = L.map("map", {
         zoomSnap: 0.1,
         center: [37.8, -96],
         zoom: 4,
@@ -30,7 +30,6 @@
         minZoom: 1,
         maxZoom: 9,
     });
-   
 
     let myData = {}
     omnivore
@@ -40,32 +39,29 @@
             myData = e.target.toGeoJSON();
             console.log(myData.features);
             for (let i of myData.features) {
-                console.log(i)
+              /*  console.log(i)
                 console.log(i.properties.Pro)
                 console.log(i.properties.Anti)
                 console.log(i.geometry.coordinates[0]) //long
-                console.log(i.geometry.coordinates[1]) //lat
+                console.log(i.geometry.coordinates[1]) //lat*/
 
             }
             for (let i of myData.features) {
                 var center = [(i.geometry.coordinates[1]), (i.geometry.coordinates[0])];
 
-                
-               /* var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                    maxZoom: 19,
-                    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-                }).addTo(map);*/
-
                 // Create a barchart
-                var myPieChart = L.minichart(center, { 
+                var myPieChart = L.minichart(center, {
                     // Add options here
                     data: [(i.properties.Pro), (i.properties.Anti)],
-                    type: 'pie', 
+                    type: 'pie',
+
+                    
                 })
-
-                mymap.addLayer(myPieChart);
-            }
-
+                map.addLayer(myPieChart);
+                var popup = L.popup()
+                .setContent("I am a popup.");
+            
+            myPieChart.bindPopup(popup).openPopup();            }
 
         })
         .on("error", function (e) {
@@ -73,68 +69,34 @@
 
         });
 
+    let otherData = {}
+    omnivore
+        .csv("data/RCV_Other.csv")
+        .on("ready", function (e) {
+        
+            console.log(e.target.toGeoJSON());
+            otherData = e.target.toGeoJSON();
+            L.geoJSON(otherData).addTo(map);
+            /*for (let i of otherData.features) {
+                console.log(i)
+                console.log(i.geometry.coordinates[0]) //long
+                console.log(i.geometry.coordinates[1]) //lat
 
-    function drawMap(data) {
-        const options = {
-            pointToLayer: function (feature, ll) {
-                return L.circleMarker(ll, {
-                    opacity: 1,
-                    weight: 1,
-                    fillOpacity: .5,
+            }
+            for (let i of otherData.features) {
+                var center = [(i.geometry.coordinates[1]), (i.geometry.coordinates[0])];}
+                var otherCities = L.circleMarker(center, {
+                    // Add options here
+                })
+                map.addLayer(otherCities);*/
 
-                });
-            },
+                
+        })
+       
 
-        };
-        // create 2 separate layers from GeoJSON data
-        const proLayer = L.geoJson(data, options).addTo(map),
-            antiLayer = L.geoJson(data, options).addTo(map);
-
-        // fit the bounds of the map to one of the layers
-        map.fitBounds(proLayer.getBounds(), {
-            padding: [50, 50],
-        });
-        // color the layers different colors
-        proLayer.setStyle({
-            color: getColor("pro"),
-        });
-        antiLayer.setStyle({
-            color: getColor("anti"),
-        });
-        resizeCircles(proLayer, antiLayer);
-
-    } // end drawMap()
-
-
-
-    function resizeCircles(proLayer, antiLayer) {
-        proLayer.eachLayer(function (layer) {
-            const radius = calcRadius(
-                Number(layer.feature.properties["Pro"])
-            );
-            layer.setRadius(radius);
-        });
-        antiLayer.eachLayer(function (layer) {
-            const radius = calcRadius(
-                Number(layer.feature.properties["Anti"])
-            );
-            layer.setRadius(radius);
-        });
-        // update the hover window with current grade's
-        retrieveInfo(antiLayer);
-    }
-
-
-    //calcradius function
-    function calcRadius(val) {
-        const radius = Math.sqrt(val / Math.PI);
-        return radius * 0.075; // adjust .5 as a scale factor
-    }
-    //end of calcradius 
+})
 
 
 
 
-    console.log(myData);
-
-})();
+    ();
