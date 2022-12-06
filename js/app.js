fc6a29d35547ad6187ad2566976e2833cc84751c
laurenkarch <lauren.karch@uky.edu>
2022-12-06 12:18:28 -0500

    poppin up poppin OFF

--- js/app.js
+++ js/app.js
@@ -32,15 +32,15 @@
     });
 
     // mapbox API parameters
-  const accessToken = `pk.eyJ1IjoibGF1cmVua2FyY2giLCJhIjoiY2wzd2RtazQyMnV2azNnbXVvcjBteHcwNyJ9.p4zlCc57GpKUgV8dPr4KxA`
-  const yourName = 'laurenkarch'
-  const yourMap = 'clbb1oa6o000b14mr6ff3vhiq'
+    const accessToken = `pk.eyJ1IjoibGF1cmVua2FyY2giLCJhIjoiY2wzd2RtazQyMnV2azNnbXVvcjBteHcwNyJ9.p4zlCc57GpKUgV8dPr4KxA`
+    const yourName = 'laurenkarch'
+    const yourMap = 'clbb1oa6o000b14mr6ff3vhiq'
 
-  // request a mapbox raster tile layer and add to map
-  L.tileLayer(`https://api.mapbox.com/styles/v1/laurenkarch/clbb1oa6o000b14mr6ff3vhiq/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibGF1cmVua2FyY2giLCJhIjoiY2wzd2RtazQyMnV2azNnbXVvcjBteHcwNyJ9.p4zlCc57GpKUgV8dPr4KxA`, {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, and <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18
-  }).addTo(map);
+    // request a mapbox raster tile layer and add to map
+    L.tileLayer(`https://api.mapbox.com/styles/v1/laurenkarch/clbb1oa6o000b14mr6ff3vhiq/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibGF1cmVua2FyY2giLCJhIjoiY2wzd2RtazQyMnV2azNnbXVvcjBteHcwNyJ9.p4zlCc57GpKUgV8dPr4KxA`, {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, and <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18
+    }).addTo(map);
 
     let myData = {}
     omnivore
@@ -50,11 +50,11 @@
             myData = e.target.toGeoJSON();
             console.log(myData.features);
             for (let i of myData.features) {
-              /*  console.log(i)
-                console.log(i.properties.Pro)
-                console.log(i.properties.Anti)
-                console.log(i.geometry.coordinates[0]) //long
-                console.log(i.geometry.coordinates[1]) //lat*/
+                /*  console.log(i)
+                  console.log(i.properties.Pro)
+                  console.log(i.properties.Anti)
+                  console.log(i.geometry.coordinates[0]) //long
+                  console.log(i.geometry.coordinates[1]) //lat*/
 
             }
             for (let i of myData.features) {
@@ -65,62 +65,80 @@
                     // Add options here
                     data: [(i.properties.Pro), (i.properties.Anti)],
                     type: 'pie',
-                    width:  0.075 * (Math.sqrt(i.properties.Pro)+Math.sqrt(i.properties.Anti)),
-                    colors: ['#7fbf7b','#fffff'],
+                    width: 0.075 * (Math.sqrt(i.properties.Pro) + Math.sqrt(i.properties.Anti)),
+                    colors: ['#7fbf7b', '#fffff'],
                     opacity: 0.75,
 
-                    
+
                 })
                 map.addLayer(myPieChart);
                 var popup = L.popup()
-                .setContent(`<class=pro>${i.properties.Locality}, ${i.properties.State}</class><br>${i.properties.Notes}`);
-            
-            myPieChart.bindPopup(popup); }
+                    .setContent(`${i.properties.Locality}, ${i.properties.State}<br>${i.properties.Notes}`);
+
+                myPieChart.bindPopup(popup);
+            }
 
         })
         .on("error", function (e) {
             console.log(e.error[0].message);
 
         });
+
+
     
-        
+
+
     let otherData = {}
     omnivore
         .csv("data/RCV_Other.csv")
         .on("ready", function (e) {
-            var markerOptions = {
-                radius: 5,
-                fillColor: "#008837",
-                color: "#000",
-                weight: 0,
-                opacity: 0.75,
-                fillOpacity: 0.75,
-
-            };
-        
             console.log(e.target.toGeoJSON());
             otherData = e.target.toGeoJSON();
-            {for (let i of otherData.features) {
-                console.log(i)
-                  console.log(i.properties.Locality)
-                  console.log(i.properties.State)
+            console.log(myData.features);
+            for (let i of otherData.features) {
+                /*  console.log(i)
+                  console.log(i.properties.Pro)
+                  console.log(i.properties.Anti)
                   console.log(i.geometry.coordinates[0]) //long
                   console.log(i.geometry.coordinates[1]) //lat*/
-  
-              }
-            L.geoJSON(otherData, {
-                pointToLayer: function (feature, latlng) {
-                    return L.circleMarker(latlng, markerOptions).bindPopup(`<class=pro>${i.properties.Locality}, ${i.properties.State}</class><br>${i.properties.Notes}`);}
-                    
-                }).addTo(map);
-            }
-        
 
-                
+            }
+            for (let i of otherData.features) {
+                var center = [(i.geometry.coordinates[1]), (i.geometry.coordinates[0])];
+
+                // Create a barchart
+                var cityMarker = L.circleMarker(center, {
+                    // Add options here
+                    center: [(i.properties.Pro), (i.properties.Anti)],
+                    radius: 5,
+                    fillColor: "#008837",
+                    color: "#000",
+                    weight: 0,
+                    opacity: 0.75,
+                    fillOpacity: 0.75,
+                    opacity: 0.75,
+
+
+                })
+                map.addLayer(cityMarker);
+                var popup = L.popup()
+                    .setContent(`${i.properties.Locality}, ${i.properties.State}<br>${i.properties.Notes}`);
+
+                cityMarker.bindPopup(popup);
+            }
+
         })
-       
+        .on("error", function (e) {
+            console.log(e.error[0].message);
+
+        });
+
+
 
 })
+
+
+
 
 
 
